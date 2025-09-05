11150d20bb7289c63e1dc9b38140701dadb72ed1
milkcoffeemilk <milk.coffee.milk@gmail.com>
2025-09-05 11:05:39 +0800

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,7 +1,11 @@
 // assets/js/config.js
 const CONFIG = {
   LIFF_ID: "2008043659-x9g3OvbJ", 
-  GAS_URL: "https://script.google.com/macros/s/AKfycbz6Nqq7OrbpZuFyZI7pYQJbdfPgR7wgCuYp5Cjy9xPJ7tbKRi3T3LShXCnOqJskiaES/exec",
+  scriptURL: "https://script.google.com/macros/s/AKfycbz6Nqq7OrbpZuFyZI7pYQJbdfPgR7wgCuYp5Cjy9xPJ7tbKRi3T3LShXCnOqJskiaES/exec",
   NOTION_API_URL: "https://你的後端API網址",
   NOTION_API_KEY: "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"
 };
+
+
+https://script.google.com/macros/s/AKfycbz6Nqq7OrbpZuFyZI7pYQJbdfPgR7wgCuYp5Cjy9xPJ7tbKRi3T3LShXCnOqJskiaES/exec
+https://script.google.com/macros/s/AKfycbz6Nqq7OrbpZuFyZI7pYQJbdfPgR7wgCuYp5Cjy9xPJ7tbKRi3T3LShXCnOqJskiaES/exec"
